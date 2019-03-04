62ee50daea64da959f2efc049731d0dd0bce56b5
hhhhojeihsu.Mac <hhhhojeihsu@gmail.com>
2019-03-04 20:38:45 +0800

    Complete Storyboard Setup

--- StupidCalculator/ViewController.h
+++ StupidCalculator/ViewController.h
@@ -10,6 +10,28 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *result;
+- (IBAction)acButton:(id)sender;
+- (IBAction)pmButton:(id)sender;
+- (IBAction)percentButton:(id)sender;
+- (IBAction)divideButton:(id)sender;
+- (IBAction)sevenButton:(id)sender;
+- (IBAction)eightButton:(id)sender;
+- (IBAction)nineButton:(id)sender;
+- (IBAction)multiButton:(id)sender;
+- (IBAction)fourButton:(id)sender;
+- (IBAction)fiveButton:(id)sender;
+- (IBAction)sixButton:(id)sender;
+- (IBAction)minusButton:(id)sender;
+- (IBAction)oneButton:(id)sender;
+- (IBAction)twoButton:(id)sender;
+- (IBAction)threeButton:(id)sender;
+- (IBAction)plusButton:(id)sender;
+- (IBAction)zeroButton:(id)sender;
+- (IBAction)twoZeroButton:(id)sender;
+- (IBAction)dotButton:(id)sender;
+- (IBAction)equalButton:(id)sender;
+
 
 @end
 
--- StupidCalculator/ViewController.m
+++ StupidCalculator/ViewController.m
@@ -20,4 +20,65 @@
 }
 
 
+- (IBAction)axButton:(id)sender {
+}
+- (IBAction)acButton:(id)sender {
+}
+
+- (IBAction)pmButton:(id)sender {
+}
+
+- (IBAction)percentButton:(id)sender {
+}
+
+- (IBAction)divideButton:(id)sender {
+}
+
+- (IBAction)sevenButton:(id)sender {
+}
+
+- (IBAction)eightButton:(id)sender {
+}
+
+- (IBAction)nineButton:(id)sender {
+}
+
+- (IBAction)multiButton:(id)sender {
+}
+
+- (IBAction)fourButton:(id)sender {
+}
+
+- (IBAction)fiveButton:(id)sender {
+}
+
+- (IBAction)sixButton:(id)sender {
+}
+
+- (IBAction)minusButton:(id)sender {
+}
+
+- (IBAction)oneButton:(id)sender {
+}
+
+- (IBAction)twoButton:(id)sender {
+}
+
+- (IBAction)threeButton:(id)sender {
+}
+
+- (IBAction)plusButton:(id)sender {
+}
+
+- (IBAction)zeroButton:(id)sender {
+}
+
+- (IBAction)twoZeroButton:(id)sender {
+}
+
+- (IBAction)dotButton:(id)sender {
+}
+
+- (IBAction)equalButton:(id)sender {
+}
 @end
